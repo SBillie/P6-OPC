3fc8d515d0a3f38798fb04316005d0c6d2044478
SBillie <savepeople26@yahoo.fr>
2022-03-16 01:07:04 +0100

    coder

--- app.js
+++ app.js
@@ -4,31 +4,33 @@ const express = require('express');
 //créer une application express
 const app = express();
 
-/*importation de morgan (logger htpp)*/
-const morgan = require("morgan");
-
-/* logger les requests et responses*/
-app.use(morgan("dev"));
 
 /*importation connexion base de données mongoDb*/
-const mongoose = require("./db/db");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://SB:0wDvLif4J6o4oZib@cluster001.ihd2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-//app.use (vient du framework express) route général et la fonction middlware
-app.use((req,res,next) => {
-    console.log("première vraie");
+
+  /*CORS Cross Origin Resource Sharing : systeme de sécurité qui empêche les requetes malveillantes*/
+/*ces headers permettent d'accéder au serveur depuis n'importe quelle origine + d'envoyer des requêtes avec les methodes GET, POST...*/
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-});
-
-app.use((req,res,next) => {
-    res.status(201);
-    next();
-});
-app.use((req,res,) => {
-    res.json({message: "ça fonctionne graaaave"});
-
-});
+  });
+  
+  app.use(express.json());
 
 
+  /*importation des routes*/
+  const userRoutes = require ("./routes/user");
+/* route auth*/
+
+app.use("api/auth", userRoutes)
 
 //exporter app.js pour accéder depuis un autre fichier
 module.exports = app;
--- db/db.js
+++
@@ -1,9 +0,0 @@
-/*importer mongoose pour la connexion à la base de données*/
-const mongoose = require('mongoose');
-mongoose.connect(
-    'mongodb+srv://NGOZONGOZO:0rC1Ux68XBqscWF8@clusterapi-piiquante.lbr95.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {userNewUrlParser: true, UnifiedTopology: true})
-.then(() => console.log("connexion à MD réussie"))
-.catch(() => console.log("connexion à MD échouée"));
-
-
-module.exports = mongoose;
